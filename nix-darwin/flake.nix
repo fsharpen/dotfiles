2871aa475658285e81359060ab0b4d2bf5d83996
Sharp Zeng <sharp.zeng@ringcentral.com>
2025-04-26 16:49:40 +0800

    Sharp's Nix

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -11,68 +11,92 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-          pkgs.vim
-          pkgs.direnv
-          pkgs.sshs
-          pkgs.glow
-          pkgs.nushell
-          pkgs.carapace
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      home-manager,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.vim
+            pkgs.direnv
+            pkgs.sshs
+            pkgs.glow
+            pkgs.nushell
+            pkgs.carapace
+            pkgs.starship
+            pkgs.nixfmt-rfc-style
+            pkgs.television
+          ];
+
+          environment.etc."pam.d/sudo_local".text = ''
+            # Managed by Nix Darwin
+            auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
+            auth       sufficient     pam_tid.so
+          '';
+
+          nix.settings.experimental-features = "nix-command flakes";
+          programs.zsh.enable = true;  # default shell on catalina
+          #programs.television.enable = true;
+          #programs.television.enableZshIntegration = true;
+
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+          system.stateVersion = 6;
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          #security.pam.services.sudo_local.touchIdAuth = true;
+
+          users.users."sharp.zeng".home = "/Users/sharp.zeng";
+          home-manager.backupFileExtension = "backup";
+
+          environment.variables = {
+            STARSHIP_CONFIG = "$HOME/.config/starship/starship.toml";
+          };
+
+          system.defaults = {
+            dock.autohide = true;
+            dock.mru-spaces = false;
+            finder.AppleShowAllExtensions = true;
+            finder.FXPreferredViewStyle = "clmv";
+            screencapture.location = "~/Pictures/screenshots";
+            screensaver.askForPasswordDelay = 10;
+          };
+
+          # Homebrew needs to be installed on its own!
+          homebrew.enable = true;
+          homebrew.casks = [
+            "font-meslo-lg-nerd-font"
+"dbeaver-community"
+            #"wireshark"
+            #"google-chrome"
+          ];
+          homebrew.brews = [
+            #"imagemagick"
+          ];
+        };
+    in
+    {
+      darwinConfigurations."LMK75JJ3F" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users."sharp.zeng" = import ./home.nix;
+          }
         ];
-      services.nix-daemon.enable = true;
-      nix.settings.experimental-features = "nix-command flakes";
-      programs.zsh.enable = true;  # default shell on catalina
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 4;
-      nixpkgs.hostPlatform = "aarch64-darwin";
-      security.pam.enableSudoTouchIdAuth = true;
-
-      users.users.omerxx.home = "/Users/omerxx";
-      home-manager.backupFileExtension = "backup";
-      nix.configureBuildUsers = true;
-      nix.useDaemon = true;
-
-      system.defaults = {
-        dock.autohide = true;
-        dock.mru-spaces = false;
-        finder.AppleShowAllExtensions = true;
-        finder.FXPreferredViewStyle = "clmv";
-        loginwindow.LoginwindowText = "devops-toolbox";
-        screencapture.location = "~/Pictures/screenshots";
-        screensaver.askForPasswordDelay = 10;
       };
 
-      # Homebrew needs to be installed on its own!
-      homebrew.enable = true;
-      homebrew.casks = [
-	      "wireshark"
-              "google-chrome"
-      ];
-      homebrew.brews = [
-	      "imagemagick"
-      ];
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."LMK75JJ3F".pkgs;
     };
-  in
-  {
-    darwinConfigurations."Omers-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [ 
-	configuration
-        home-manager.darwinModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.omerxx = import ./home.nix;
-        }
-      ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."Omers-MacBook-Pro".pkgs;
-  };
 }
--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -4,30 +4,42 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "omerxx";
-  home.homeDirectory = "/Users/omerxx";
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+  home.username = "sharp.zeng";
+  home.homeDirectory = "/Users/sharp.zeng";
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
 # Makes sense for user specific applications that shouldn't be available system-wide
   home.packages = [
+    pkgs.wezterm
+    pkgs.skhd
+    pkgs.zellij
+    pkgs.tmux
+    pkgs.sketchybar
+    pkgs.atuin
+    pkgs.alacritty
+    pkgs.bat
+    pkgs.silver-searcher
+    pkgs.kubectl
+    pkgs.k9s
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # ".zshrc".source = ~/dotfiles/zshrc/.zshrc;
-    # ".config/wezterm".source = ~/dotfiles/wezterm;
-    # ".config/skhd".source = ~/dotfiles/skhd;
-    # ".config/starship".source = ~/dotfiles/starship;
-    # ".config/zellij".source = ~/dotfiles/zellij;
-    # ".config/nvim".source = ~/dotfiles/nvim;
-    # ".config/nix".source = ~/dotfiles/nix;
-    # ".config/nix-darwin".source = ~/dotfiles/nix-darwin;
-    # ".config/tmux".source = ~/dotfiles/tmux;
-    # ".config/ghostty".source = ~/dotfiles/ghostty;
-    # ".config/aerospace".source = ~/dotfiles/aerospace;
-    # ".config/sketchybar".source = ~/dotfiles/sketchybar;
-    # ".config/nushell".source = ~/dotfiles/nushell;
+   # ".zshrc".source = ~/dotfiles/zshrc/.zshrc;
+   # ".config/atuin".source = ~/dotfiles/atuin;
+   # ".config/wezterm".source = ~/dotfiles/wezterm;
+   # ".config/skhd".source = ~/dotfiles/skhd;
+   # ".config/starship".source = ~/dotfiles/starship;
+   # ".config/zellij".source = ~/dotfiles/zellij;
+   # ".config/nvim".source = ~/dotfiles/nvim;
+   # ".config/nix".source = ~/dotfiles/nix;
+   # ".config/nix-darwin".source = ~/dotfiles/nix-darwin;
+   # ".config/tmux".source = ~/dotfiles/tmux;
+   # ".config/ghostty".source = ~/dotfiles/ghostty;
+   # ".config/aerospace".source = ~/dotfiles/aerospace;
+   # ".config/sketchybar".source = ~/dotfiles/sketchybar;
+   # ".config/nushell".source = ~/dotfiles/nushell;
   };
 
   home.sessionVariables = {
@@ -46,6 +58,8 @@
       if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
         . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
       fi
+      eval "$(${pkgs.starship}/bin/starship init zsh)"
+      export KUBECONFIG=$HOME/dotfiles/kubectl/config
     '';
   };
 }
